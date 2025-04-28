8e49e26f3f174a493ce9d11e27737cfad147c1fe
Raichu15678 <matefekete355@gmail.com>
2025-04-28 09:41:26 +0200

    Add output div to evkeres.html and update kereses function to display results

--- evkeres.js
+++ evkeres.js
@@ -1,3 +1,18 @@
 function kereses() {
-    
+    let beEv=document.getElementById("beEv").value
+    fetch("https://api.nobelprize.org/v1/prize.json")
+    .then(x=>x.json())
+    .then(y=>{
+        y.prizes.forEach(elem => {
+            if (elem.year==beEv) {
+                
+                sz+=`
+                <p>
+                ${elem.year} ${elem.category}
+                </p>
+                `
+            }
+        });
+        document.getElementById("kiadat").innerHTML=sz
+    })
 }
