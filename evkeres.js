fb66a15a30350f63a951ae69f3b0b998f4ece207
Raichu15678 <matefekete355@gmail.com>
2025-04-28 09:42:48 +0200

    Add missing div for results display in evkeres.html and initialize result string in kereses function

--- evkeres.js
+++ evkeres.js
@@ -3,6 +3,7 @@ function kereses() {
     fetch("https://api.nobelprize.org/v1/prize.json")
     .then(x=>x.json())
     .then(y=>{
+        let sz=""
         y.prizes.forEach(elem => {
             if (elem.year==beEv) {
                 
